f330cb1e1b3b0f5278d51c714090608890e2168a
Víctor Guzmán <fawques@gmail.com>
2014-10-05 17:52:03 +0100

    access to Dropbox API

--- dropboxAccount.py
+++ dropboxAccount.py
@@ -1,8 +1,25 @@
 import account
+import dropbox
+import os
+
+app_key = os.getenv("APP_KEY")
+app_secret = os.getenv("APP_SECRET")
 
 
 class DropboxAccount(account.Account):
     """docstring for DropboxAccount"""
-    def __init__(self, arg):
-        super(DropboxAccount, self).__init__()
-        self.arg = arg
+    def __init__(self, user):
+        self.user = user
+        self.startOAuthFlow()
+
+    def startOAuthFlow(self):
+        flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
+        # Have the user sign in and authorize this token
+        authorize_url = flow.start()
+        print('1. Go to: ' + authorize_url)
+        print('2. Click "Allow" (you might have to log in first)')
+        print('3. Copy the authorization code.')
+        code = input("Enter the authorization code here: ").strip()
+        self.access_token, self.user_id = flow.finish(code)
+        # TODO: guardar el token y user_id de forma segura
+        self.client = dropbox.client.DropboxClient(self.access_token)
