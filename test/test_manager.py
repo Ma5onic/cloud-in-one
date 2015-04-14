e738f2be482cf558d1b33825e6db685c6ed854db
Víctor Guzmán Pedrajas <victor.pedrajas@logicinfo.com>
2015-04-14 18:10:31 +0100

    creating tests for manager. Half done...

--- test/test_manager.py
+++ test/test_manager.py
@@ -9,13 +9,60 @@ from nose.tools import assert_false
 
 class TestManager(object):
     def __init__(self):
-        pass
+        self.config_default = {"sync_folder_name": "./test/sync_folder","database": "db/tests.db"}
+        self.man = None
+
+    @classmethod
+    def setup_class(klass):
+        """This method is run once for each class before any tests are run"""
+
+    @classmethod
+    def teardown_class(klass):
+        """This method is run once for each class _after_ all tests are run"""
+
+    def setUp(self):
+        """This method is run once before _each_ test method is executed"""
+        self.man = manager.Manager('user', 'password',self.config_default)
+
+    def teardown(self):
+        """This method is run once after _each_ test method is executed"""
+        for i in self.man.database.tables:
+            self.man.database[i].drop()
+
+        self.man = None
+
 
     def test_manager(self):
-        man = manager.Manager('user', 'password')
-        assert_false(man is None)
+        assert_true(self.man)
 
     def test_newAccount_dropbox(self):
-        man = manager.Manager('user', 'password')
-        man.newAccount('dropbox_stub', 'user')
-        assert_true(man.cuentas)
+        self.man.newAccount('dropbox_stub', 'user')
+        assert_true(self.man.cuentas)
+        
+    def test_deleteAccount(self):
+        self.man.newAccount('dropbox_stub', 'user')
+        self.man.deleteAccount(self.man.cuentas[0])
+        assert_false(self.man.cuentas)
+
+    # def test_updateLocalSyncFolder(self):
+
+    # TODO: several deltas...
+    def test_findRemoteChanges(self):
+        self.man.newAccount('dropbox_stub', 'user')
+        remoteChanges = self.man.findRemoteChanges()
+
+        expected_remoteChanges = [{'path': '/test/muerte.txt','hash':'MISSING','account':self.man.cuentas[0]}]
+        assert_equal(remoteChanges, expected_remoteChanges)
+
+        
+    def test_findLocalChanges(self):
+        pass
+        
+    def test_applyLocalChanges(self):
+        pass
+        
+    def test_applyRemoteChanges(self):
+        pass
+        
+
+
