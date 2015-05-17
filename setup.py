409b3afb17094c1f0184f08f664f1a3a42d5d03b
Víctor Guzmán <fawques@gmail.com>
2015-05-17 18:23:43 +0200

    small fix in setup.py for py2exe 32 bits

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 from distutils.core import setup
-import py2exe, sys, os
-
-sys.argv.append('py2exe')
+import py2exe
 
 setup(
-    options = {'py2exe': {'bundle_files': 2, "includes": ["pygments.styles.default", "sqlalchemy.dialects.sqlite"]}},
+    options = {'py2exe': {'bundle_files': 1, "includes": ["sqlalchemy.dialects.sqlite"], "excludes": ["six.moves.urllib.parse"]}},
     #windows = [{'script': "hello.py"}],
     console = ["manager.py"],
     zipfile = None,
