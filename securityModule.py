db210158b2537d3444b94a77c2ec145f74dd7807
Víctor Guzmán <fawques@gmail.com>
2015-05-25 13:04:17 +0200

    created encrypt and decrypt methods in securityModule. Hashing password with SHA-256 and hardcoded salt

--- securityModule.py
+++ securityModule.py
@@ -1,5 +1,21 @@
+from io import BytesIO
+import hashlib
+import simplecrypt
+
+
 class SecurityModule():
     """docstring for SecurityModule"""
-    def __init__(self, arg):
+    def __init__(self, password):
         super(SecurityModule, self).__init__()
-        self.arg = arg
+        # we cannot use a random salt because this should be installed in several computers and give the same password to be able to decrypt...
+        self.password = hashlib.sha256(('th¡5iS@sal7' + password).encode('utf-8')).hexdigest()
+
+    def encrypt(self, streamFile):
+        encrypted_IO = BytesIO(simplecrypt.encrypt(self.password, streamFile.read()))
+        streamFile.close()
+        return encrypted_IO
+
+    def decrypt(self, streamFile):
+        decrypted_IO = BytesIO(simplecrypt.decrypt(self.password, streamFile.read()))
+        streamFile.close()
+        return decrypted_IO
