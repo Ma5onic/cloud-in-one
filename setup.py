e4d39f0070e7f7d7c10b0199909d05aa61a5fc5e
Víctor Guzmán <fawques@gmail.com>
2015-06-05 18:34:58 +0200

    create cio-crypt.exe, changed main exe to cloud-in-one.exe

--- setup.py
+++ setup.py
@@ -2,9 +2,19 @@ from distutils.core import setup
 import py2exe
 
 setup(
-    options = {'py2exe': {'bundle_files': 1, "includes": ["sqlalchemy.dialects.sqlite"], "excludes": ["six.moves.urllib.parse"]}},
+    options={'py2exe': {'bundle_files': 1, "includes": ["sqlalchemy.dialects.sqlite"], "excludes": ["six.moves.urllib.parse"]}},
     #windows = [{'script': "hello.py"}],
-    console = ["main.py"],
-    zipfile = None,
-    data_files = [('config', ['config/config.json', 'config/log.json']), ('', ['trusted-certs.crt'])]
+    console=[{
+        ### Main Python script
+        "script": "main.py",
+        # "icon_resources": [(0, "favicon.ico")], ### Icon to embed into the PE file.
+        "dest_base": "cloud-in-one"
+    }, {
+        ### Main Python script
+        "script": "main_crypto.py",
+        # "icon_resources": [(0, "favicon.ico")], ### Icon to embed into the PE file.
+        "dest_base": "cio-crypt"
+    }],
+    zipfile=None,
+    data_files=[('config', ['config/config.json', 'config/log.json']), ('', ['trusted-certs.crt'])]
 )
